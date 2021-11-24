55260f0c4d1faf817aeec754faea6f4c005671b4
Yaw huei <91109301+YawHuei@users.noreply.github.com>
2021-11-24 17:43:50 +0800

    fix prefix error
    Rename folder [grub2] to [grub]
    Modify related content

--- grubIso/grub2_cd.bat
+++ grubIso/grub2_cd.bat
@@ -3,9 +3,9 @@ cls
 cd /d %~dp0
 set "tcd=%cd:\=/%"
 
-pushd ".\grub2\"
+pushd ".\grub\"
 
-grub-mkimage.exe -c ..\cfg\grub_cd.cfg -d i386-pc -p /grub2 -o core.img -O i386-pc linux16 linux normal iso9660 biosdisk memdisk search tar ls
+grub-mkimage.exe -c ..\cfg\grub_cd.cfg -d i386-pc -p /grub -o core.img -O i386-pc linux16 linux normal iso9660 biosdisk memdisk search tar ls
 timeout.exe /t 2 /nobreak >nul
 
 if exist ..\grub2.img del /f /q ..\grub2.img >nul
@@ -18,7 +18,7 @@ if exist .\grub2.iso del /f /q .\grub2.iso >nul
 
 .\xorriso\xorriso.exe -as mkisofs -iso-level 3 -full-iso9660-filenames -eltorito-boot grub/grub2.img ^
  -no-emul-boot -boot-load-size 4 -boot-info-table --eltorito-catalog grub/boot.cat --grub2-boot-info ^
- --grub2-mbr grub2/i386-pc/boot_hybrid.img -eltorito-alt-boot -output "%tcd%/grub2.iso" -graft-points "grub=./grub2" ^
+ --grub2-mbr grub/i386-pc/boot_hybrid.img -eltorito-alt-boot -output "%tcd%/grub2.iso" -graft-points "grub=./grub" ^
   grub/grub2.img=grub2.img
 
 pause
